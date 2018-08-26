be9a7b0c862410995879ebc210668e41925ea06d
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-08-26 23:18:45 +0100

    Adding skeleton downloader

---
+++ Auctioneer/Auctioneer.fs
@@ -0,0 +1,29 @@
+﻿namespace Auctioneer
+
+type 'a Agent = 'a MailboxProcessor
+
+module Downloader =
+
+    type private DownloadMessage =
+        | AddRealm of string
+        | DownloadAuctions
+
+    let private agent = Agent<_>.Start(fun inbox ->
+        let rec loop (realms : Set<string>) =
+            async {
+                let! message = inbox.Receive ()
+                match message with
+                | AddRealm realm ->
+                    return! loop <| Set.add realm realms
+                | DownloadAuctions ->
+                    //Go through each realm, download auction data, stash in DB.
+                    return! loop realms
+            }
+        loop Set.empty
+    )
+
+    let add realm =
+        agent.Post (AddRealm realm)
+
+    let downloadLatest () =
+        agent.Post DownloadAuctions
--- Auctioneer/Checker.fs
+++ Auctioneer/Checker.fs
@@ -3,13 +3,6 @@
 type Realm = string
 type InsertionTime = int64
 
-module Option =
-
-    let apply (a : 'a option) (f : ('a -> 'b) option) =
-        match f with
-        | None -> None
-        | Some f -> Option.map f a
-
 module InsertionCheck =
 
     type private InsertionMessage =
@@ -17,32 +10,31 @@ module InsertionCheck =
         | IsValidInsertion of (Realm * InsertionTime * AsyncReplyChannel<bool>)
         | UpdateLastInsertion of (Realm * InsertionTime)
 
-    let private agent =
-        MailboxProcessor<InsertionMessage>.Start(fun inbox ->
-            let rec loop (lastInsert : Map<Realm, InsertionTime>) =
-                async {
-                    let! message = inbox.Receive ()
-                    match message with
-                    | FetchLastInsertion ->
-                        //Go to the database, grab the last insert for each realm
-                        return! loop lastInsert
-                    | IsValidInsertion (realm, currentInsert, channel) ->
-                        match Map.tryFind realm lastInsert with
-                        | None -> channel.Reply true
-                        | Some v ->
-                            v < currentInsert
-                            |> channel.Reply
-                        return! loop lastInsert
-                    | UpdateLastInsertion (realm, currentInsert) ->
-                        //Push this insert into the database, if it fails -> add a message to retry
-                        match Map.tryFind realm lastInsert with
-                        | None ->
-                            return! loop <| Map.add realm currentInsert lastInsert
-                        | Some v ->
-                            return! loop <| Map.add realm (max v currentInsert) lastInsert
-                }
-            loop Map.empty
-        )
+    let private agent = MailboxProcessor<_>.Start(fun inbox ->
+        let rec loop (lastInsert : Map<Realm, InsertionTime>) =
+            async {
+                let! message = inbox.Receive ()
+                match message with
+                | FetchLastInsertion ->
+                    //Go to the database, grab the last insert for each realm
+                    return! loop lastInsert
+                | IsValidInsertion (realm, currentInsert, channel) ->
+                    match Map.tryFind realm lastInsert with
+                    | None -> channel.Reply true
+                    | Some v ->
+                        v < currentInsert
+                        |> channel.Reply
+                    return! loop lastInsert
+                | UpdateLastInsertion (realm, currentInsert) ->
+                    //Push this insert into the database, if it fails -> add a message to retry
+                    match Map.tryFind realm lastInsert with
+                    | None ->
+                        return! loop <| Map.add realm currentInsert lastInsert
+                    | Some v ->
+                        return! loop <| Map.add realm (max v currentInsert) lastInsert
+            }
+        loop Map.empty
+    )
 
     let fetchLatest () =
         agent.Post FetchLastInsertion
